666f985d1a2aee31f42fff1f8445c05ab4a52031
Aleksandra Ujvari <ujvari65@gmail.com>
2022-09-12 18:22:28 +0200

    Create animal table

--- queries.sql
+++ queries.sql
@@ -1,3 +1,15 @@
-/*Queries that provide answers to the questions from all projects.*/
+SELECT * FROM animals WHERE Name like '%mon';
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animals WHERE Date_of_birth >= '2016-01-01' AND Date_of_birth <= '2019-12-31';
+
+SELECT * FROM animals WHERE Escape_attempts < 3 AND Neutered = 'true';
+
+SELECT * FROM animals WHERE Name = 'Agumon' OR Name = 'Pikachu';
+
+SELECT Name, Escape_attempts FROM animals where weight_kg > 10.5;
+
+SELECT * FROM animals WHERE Neutered = 'true';
+
+SELECT * FROM animals WHERE Name != 'Gabumon';
+
+SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
