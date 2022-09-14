92f451e0e3832c8a63bea74a3200818614dd9270
Aleksandra Ujvari <ujvari65@gmail.com>
2022-09-14 12:59:02 +0200

    Add, remove, update table animals and answer the query questions using JOIN

--- data.sql
+++ data.sql
@@ -34,3 +34,46 @@ VALUES
 UPDATE animals SET Species = 'digimon' WHERE Name like '%mon';
 
 UPDATE animals SET Species = 'pokemon' WHERE Species = 'unspecified';
+
+INSERT INTO owners
+  (full_name, age)
+VALUES
+  ('Sam Smith ', 34),
+  ('Jennifer Orwell', 19 ),
+  ('Bob', 45 ),
+  ('Melody Pond', 77 ),
+  ('Dean Winchester', 14 ),
+  ('Jodie Whittaker', 38 );
+
+INSERT INTO species
+  (name)
+VALUES
+  ('Pokemon'),
+  ('Digimon');
+
+UPDATE animals SET species_id = (SELECT id
+FROM species
+WHERE name = 'Digimon') WHERE name LIKE '%mon';
+
+UPDATE animals SET species_id = (SELECT id
+FROM species
+WHERE name = 'Pokemon') WHERE name NOT LIKE '%mon';
+
+
+UPDATE animals SET owner_id = (SELECT id
+FROM owners
+WHERE full_name = 'Sam Smith') WHERE name = 'Agumon';
+
+UPDATE animals SET owner_id = (SELECT id
+FROM owners
+WHERE full_name = 'Jennifer Orwell') WHERE name = 'Gabumon' OR name = 'Pikachu';
+
+UPDATE animals SET owner_id = (SELECT id
+FROM owners
+WHERE full_name = 'Bob') WHERE name = 'Devimon' OR name = 'Plantmon';
+
+UPDATE animals SET owner_id = (SELECT id
+FROM owners
+WHERE full_name = 'Melody Pond') WHERE name = 'Charmander' OR name = 'Squirtle' OR name = 'Blossom';
+
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Dean Winchester') WHERE name = 'Angemon' OR name = 'Boarmon';
--- schema.sql
+++ schema.sql
@@ -6,6 +6,41 @@ CREATE TABLE animals
     Escape_attempts int,
     Neutered boolean,
     Weight_kg decimal,
+    PRIMARY KEY (ID),
 );
 
 ALTER TABLE animals ADD Species varchar(100);
+
+ALTER TABLE animals DROP COLUMN ID;
+ALTER TABLE animals ADD ID SERIAL PRIMARY KEY;
+
+CREATE TABLE owners
+(
+    ID int,
+    full_name VARCHAR(250),
+    age INT,
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE species
+(
+    ID int,
+    full_name VARCHAR(100),
+    PRIMARY KEY (ID)
+);
+
+ALTER TABLE animals DROP COLUMN species;
+SELECT *
+FROM animals;
+
+ALTER TABLE animals ADD species_id INT;
+SELECT *
+FROM animals;
+
+ALTER TABLE animals ADD owner_id INT;
+SELECT *
+FROM animals;
+
+ALTER TABLE animals ADD CONSTRAINT FK_species_table FOREIGN KEY(species_id) REFERENCES species(id);
+ALTER TABLE animals ADD CONSTRAINT FK_owners_table FOREIGN KEY(owner_id) REFERENCES owners(id);
+
