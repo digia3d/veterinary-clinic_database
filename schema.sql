b6ec45029d3e40535b5f39ef70ca4c80a7f53d35
Aleksandra Ujvari <ujvari65@gmail.com>
2022-09-19 09:58:48 +0200

    Add data queries

--- schema.sql
+++ schema.sql
@@ -63,3 +63,11 @@ CREATE TABLE visits (
     FOREIGN KEY (vets_id) REFERENCES vets(id),
     PRIMARY KEY (id)
 );
+
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
+SELECT COUNT(*) FROM visits where animal_id = 4;
+SELECT * FROM visits where vet_id = 2;
+SELECT * FROM owners where email = 'owner_18327@mail.com';
+
+
