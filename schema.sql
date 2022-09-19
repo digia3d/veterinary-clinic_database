c85e2f78fb8ef1fef8be3eec11a3102163f6d74e
Sevenpros <sethbisoso@gmail.com>
2022-09-19 16:24:14 +0200

    Improve excution time

--- schema.sql
+++ schema.sql
@@ -54,15 +54,8 @@ CREATE TABLE specializations (
     FOREIGN kEY (vets_id) REFERENCES vets(id),
     PRIMARY kEY (species_id, vets_id)
 );
-CREATE TABLE visits (
-    animals_id INT,
-    vets_id INT,
-    date_of_visit DATE,
-    id INT GENERATED ALWAYS AS IDENTITY,
-    FOREIGN KEY (animals_id) REFERENCES animals(id),
-    FOREIGN KEY (vets_id) REFERENCES vets(id),
-    PRIMARY KEY (id)
-);
+
+
 
 ALTER TABLE owners ADD COLUMN email VARCHAR(120);
 
@@ -70,4 +63,6 @@ SELECT COUNT(*) FROM visits where animal_id = 4;
 SELECT * FROM visits where vet_id = 2;
 SELECT * FROM owners where email = 'owner_18327@mail.com';
 
-
+CREATE INDEX animals_animals_id_desc ON visits(animals_id DESC);
+CREATE INDEX vets_id_asc ON visits(vets_id ASC);
+CREATE INDEX email_desc ON owners(email DESC);
